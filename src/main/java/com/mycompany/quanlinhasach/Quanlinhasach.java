995a8dbfe1a2c43d70683e1144e6b54d874d7c7d
DatNguyn <dattnguyen602@gmail.com>
2024-12-14 12:01:20 +0700

    first commit

---
+++ src/main/java/com/mycompany/quanlinhasach/Quanlinhasach.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quanlinhasach;
+
+/**
+ *
+ * @author TienDat
+ */
+public class Quanlinhasach {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
